bf81119264e246ac035603f5b6f5a912b53fb970
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2020-04-29 15:58:53 +0200

    Temporary check in

---
+++ DotNet/src/Math/Math.FSharp/Helpers.fs
@@ -0,0 +1,24 @@
+﻿namespace SFX.FSharp.Math
+
+[<AutoOpen>]
+module Helpers =
+
+    let internal either isValid succeed fail x =
+        match x |> isValid with
+        | false -> x |> succeed
+        | _ -> fail()
+    let internal either2 isValid succeed fail x y =
+        match x |> isValid, y |> isValid with
+        | false, false -> succeed x y
+        | _ -> fail()
+
+
+    let internal infiniteAct acc f stop x =
+        let sequence = 
+            Seq.initInfinite()
+        0
+    //let internal exp' one inc add mul stop x =
+    //    let start = add one x
+    //    let two = inc one
+
+
---
+++ DotNet/src/Math/Math.FSharp/Script1.fsx
@@ -0,0 +1,7 @@
+﻿let x = bigint(100)
+let y = bigint(22)
+
+let gcd = bigint.GreatestCommonDivisor(x,y)
+let div, rem = bigint.DivRem(x,y)
+
+let gcd' = bigint.GreatestCommonDivisor(bigint(-10),bigint(20))
