42d8fef68f93942e6031e170403b9b5bbc82e6c5
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2020-09-11 20:29:20 +0200

    Upgraded SDK and deps

--- DotNet/src/Math/Math.FSharp/Rational.fs
+++ DotNet/src/Math/Math.FSharp/Rational.fs
@@ -30,32 +30,32 @@ module Rational =
         Numerator = x.Numerator*y.Denominator+y.Numerator*x.Denominator
         Denominator = x.Denominator*y.Denominator
     }
-    let tryAdd = either2 isNan (fun x y -> add' x y |> succeedAsResult) failAsResult
-    let add = either2 isNan add' failWithException
+    //let tryAdd = either2 isNan (fun x y -> add' x y |> succeedAsResult) failAsResult
+    //let add = either2 isNan add' failWithException
 
     // subtract
     let private subtract' x y = {
         Numerator = x.Numerator*y.Denominator+y.Numerator*x.Denominator
         Denominator = x.Denominator*y.Denominator
     }
-    let trySubtract x y = either2 isNan (fun x y -> subtract' x y |> succeedAsResult) failAsResult 
-    let subtract x y = either2 isNan subtract' failWithException
+    //let trySubtract x y = either2 isNan (fun x y -> subtract' x y |> succeedAsResult) failAsResult 
+    //let subtract x y = either2 isNan subtract' failWithException
 
     // multiply
     let private multiply' x y = {
         Numerator = x.Numerator*y.Numerator
         Denominator = x.Denominator*y.Denominator
     }
-    let tryMultiply x y = either2 isNan (fun x y -> multiply' x y |> succeedAsResult) failAsResult 
-    let multiply x y = either2 isNan multiply' failWithException
+    //let tryMultiply x y = either2 isNan (fun x y -> multiply' x y |> succeedAsResult) failAsResult 
+    //let multiply x y = either2 isNan multiply' failWithException
 
     // divide
     let private divide' x y = {
         Numerator = x.Numerator*y.Denominator
         Denominator = x.Denominator*y.Numerator
     }
-    let tryDivide x y = either2 isNan (fun x y -> divide' x y |> succeedAsResult) failAsResult 
-    let divide x y = either2 isNan divide' failWithException
+    //let tryDivide x y = either2 isNan (fun x y -> divide' x y |> succeedAsResult) failAsResult 
+    //let divide x y = either2 isNan divide' failWithException
 
 
 
